43204ecdba652b3acd6b629241c222803ec703f9
nuriu <nuriu@users.noreply.github.com>
2016-05-08 17:59:44 +0300

    proje oluşturuldu

---
+++ gunluk.sh
@@ -0,0 +1,1 @@
+git log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
---
+++ src/is/Kontrolcu.java
@@ -0,0 +1,4 @@
+package is;
+
+public class Kontrolcu {
+}
