4430ec8aa302583c9e6222fcf560639fe81f751d
nuriu <nuriu@users.noreply.github.com>
2016-05-09 13:08:47 +0300

    kontrolcüler oluşturuldu.

---
+++ src/uygulama/ElemanKontrolcusu.java
@@ -0,0 +1,11 @@
+package uygulama;
+
+/**
+ * Paket:           uygulama
+ * Oluşturan:       Nuri UZUNOĞLU
+ * Oluşturma:       09.05.2016 | 13:04
+ */
+
+public class ElemanKontrolcusu {
+
+}
---
+++ src/uygulama/SirketKontrolcusu.java
@@ -0,0 +1,10 @@
+package uygulama;
+
+/**
+ * Paket:           uygulama
+ * Oluşturan:       Nuri UZUNOĞLU
+ * Oluşturma:       09.05.2016 | 13:05
+ */
+
+public class SirketKontrolcusu {
+}
